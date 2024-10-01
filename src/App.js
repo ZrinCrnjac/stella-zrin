263895541eba3a633eccc838c17c60588bc5ec5a
ZrinCrnjac <zrin.crnjac@gmail.com>
2024-10-01 22:40:45 +0200

    Added new pictures, style, option to add a second guest

--- src/App.js
+++ src/App.js
@@ -7,23 +7,55 @@ function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [isComing, setIsComing] = useState('');
-  const [message, setMessage] = useState('');
+  const [bringingGuest, setBringingGuest] = useState(false); // Track if a guest is being brought
+  const [guestFirstName, setGuestFirstName] = useState(''); // Guest's first name
+  const [guestLastName, setGuestLastName] = useState(''); // Guest's last name
+
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [responseMessage, setResponseMessage] = useState('');
   const [responseImage, setResponseImage] = useState('');
+  
+  // New state variable to toggle the form visibility
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // Validate input fields
     if (firstName && lastName && isComing) {
       try {
-        await addDoc(collection(db, "guests"), {
+        // Create main guest data
+        const guestData = {
           firstName,
           lastName,
           isComing: isComing === 'yes',
-        });
+        };
+  
+        // Add main guest to the database
+        await addDoc(collection(db, "guests"), guestData);
+  
+        // Check if a guest is being brought
+        if (bringingGuest) {
+          // Validate guest's information
+          if (guestFirstName && guestLastName) {
+            // Create guest data
+            const extraGuestData = {
+              firstName: guestFirstName,
+              lastName: guestLastName,
+              isComing: true, // Automatically set guest attendance to true
+            };
+  
+            // Add guest's data to the database
+            await addDoc(collection(db, "guests"), extraGuestData);
+          } else {
+            setResponseMessage('Please fill out guest information.');
+            return; // Stop execution if guest information is incomplete
+          }
+        }
+  
+        // Set form submission state
         setFormSubmitted(true);
-
-        // Set response message and image based on the selection
+  
+        // Set response message based on attendance status
         if (isComing === 'yes') {
           setResponseMessage('Veselimo se vašem dolasku!');
           setResponseImage('https://media.istockphoto.com/id/1142467597/vector/champagne-glass-vector-icon.jpg?s=612x612&w=0&k=20&c=6oXG-jkPEqaqgOfiKbgTxHzj7COX53Gku3o52H_KV3Y=');
@@ -32,10 +64,11 @@ function App() {
           setResponseImage('https://t3.ftcdn.net/jpg/01/12/43/90/360_F_112439022_Sft6cXK9GLnzWjjIkVMj2Lt34RcKUpxm.jpg');
         }
       } catch (error) {
-        setMessage('Error submitting RSVP. Please try again.');
+        console.error("Error submitting RSVP:", error); // Log the error for debugging
+        setResponseMessage('Error submitting RSVP. Please try again.');
       }
     } else {
-      setMessage('Please complete the form.');
+      setResponseMessage('Please complete the form.');
     }
   };
 
@@ -43,10 +76,13 @@ function App() {
     setFirstName('');
     setLastName('');
     setIsComing('');
+    setBringingGuest(false);
+    setGuestFirstName('');
+    setGuestLastName('');
     setFormSubmitted(false);
-    setMessage('');
     setResponseMessage('');
     setResponseImage('');
+    setIsFormVisible(false); // Reset form visibility
   };
 
   return (
@@ -62,70 +98,126 @@ function App() {
           </div>
           <div className="right-side">
             <div className="wedding-text">
-              <h2 className="wedding-intro">
-                Ljudi su oni koji trenutke čine posebnima, pozivamo vas da uljepšate naše vjenčanje svojim dolaskom
-              </h2>
-              <p className="wedding-details">
-                11. TRAVNJA 2025.<br />
-                <div className="decorative-elements">&#x2766;&#x2766;&#x2766;</div>
-                <span className="wedding-names">Stella & Zrin</span>
-                <div className="decorative-elements">&#x2766;&#x2766;&#x2766;</div>
-                <br />
-                18:00 Okupljanje pred zgradom Županije u Osijeku<br />
-                19:00 Zoo Hotel Riverside
-              </p>
-
-              <form onSubmit={handleSubmit} className="rsvp-form">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Ime:</td>
-                      <td>
-                        <input
-                          type="text"
-                          placeholder="Ime"
-                          value={firstName}
-                          onChange={(e) => setFirstName(e.target.value)}
-                          required
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Prezime:</td>
-                      <td>
-                        <input
-                          type="text"
-                          placeholder="Prezime"
-                          value={lastName}
-                          onChange={(e) => setLastName(e.target.value)}
-                          required
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Dolazite?</td>
-                      <td>
-                        <select
-                          value={isComing}
-                          onChange={(e) => setIsComing(e.target.value)}
-                          required
-                        >
-                          <option value="">Odaberite</option>
-                          <option value="yes">Da</option>
-                          <option value="no">Ne</option>
-                        </select>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colSpan="2">
-                        <button type="submit">Submit</button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </form>
-
-              {message && <p>{message}</p>}
+              {!isFormVisible ? ( // Toggle between wedding info and form
+                <>
+                  <h2 className="wedding-intro">
+                    Ljudi su oni koji trenutke čine posebnima, pozivamo vas da uljepšate naše vjenčanje svojim dolaskom
+                  </h2>
+                  <p className="wedding-details">
+                    11. TRAVNJA 2025.<br />
+                    <div className="decorative-elements">
+                      <img src="https://i.imgur.com/CShKlBl.png" alt="Stella and Zrin" className="wedding-names-image" />
+                    </div>
+                    <br />
+                  </p>
+                  <div className="wedding-info">
+                    18:00 Okupljanje pred zgradom Županije u Osijeku<br />
+                    19:00 Zoo Hotel Riverside
+                  </div>
+                  <button className="responseButton" onClick={() => setIsFormVisible(true)}>Odgovori</button> {/* Button to show the form */}
+                  <p className="wedding-info2">
+                    Molimo da na pozivnicu odgovorite najkasnije do 1. ožujka 2025. godine
+                  </p>
+                </>
+              ) : ( // Form section
+                <>
+                  <button className="responseButton" onClick={() => setIsFormVisible(false)}>Natrag</button> {/* Button to go back to wedding info */}
+                  {!bringingGuest && (
+                    <img src="https://i.imgur.com/H3JiRGH.png" alt="Additional Info" className="additional-image" />
+                  )}
+                  <form 
+                    onSubmit={handleSubmit} 
+                    className={`rsvp-form ${bringingGuest ? 'guest-selected' : ''}`} 
+                  >
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Ime:</td>
+                          <td>
+                            <input
+                              type="text"
+                              placeholder="Ime"
+                              value={firstName}
+                              onChange={(e) => setFirstName(e.target.value)}
+                              required
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Prezime:</td>
+                          <td>
+                            <input
+                              type="text"
+                              placeholder="Prezime"
+                              value={lastName}
+                              onChange={(e) => setLastName(e.target.value)}
+                              required
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Dolazite?</td>
+                          <td>
+                            <select
+                              value={isComing}
+                              onChange={(e) => setIsComing(e.target.value)}
+                              required
+                            >
+                              <option value="">Odaberite</option>
+                              <option value="yes">Da</option>
+                              <option value="no">Ne</option>
+                            </select>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Vodite li pratnju?</td>
+                          <td>
+                            <input
+                              type="checkbox"
+                              checked={bringingGuest}
+                              onChange={(e) => setBringingGuest(e.target.checked)}
+                            />
+                          </td>
+                        </tr>
+                        {bringingGuest && (
+                          <>
+                            <tr>
+                              <td>Ime pratnje:</td>
+                              <td>
+                                <input
+                                  type="text"
+                                  placeholder="Ime pratnje"
+                                  value={guestFirstName}
+                                  onChange={(e) => setGuestFirstName(e.target.value)}
+                                  required
+                                />
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Prezime pratnje:</td>
+                              <td>
+                                <input
+                                  type="text"
+                                  placeholder="Prezime pratnje"
+                                  value={guestLastName}
+                                  onChange={(e) => setGuestLastName(e.target.value)}
+                                  required
+                                />
+                              </td>
+                            </tr>
+                          </>
+                        )}
+                        <tr>
+                          <td colSpan="2">
+                            <button type="submit" className="responseButton">Odgovori</button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </form>
+                </>
+              )}
+              {responseMessage && <p>{responseMessage}</p>}
             </div>
           </div>
         </>
@@ -141,7 +233,7 @@ function App() {
           <div className="right-side">
             <h2>{responseMessage}</h2>
             {responseImage && <img src={responseImage} alt="Response" className="response-image" />}
-            <button className="reset-button" onClick={handleReset}>
+            <button className="responseButton" onClick={handleReset}>
               Ako želite opet ispuniti obrazac kliknite ovdje
             </button>
           </div>
